314b986372ec16d48ef2dd4c5d296030506b18f3
Rajia Sultana Shraboni <shraboni3103@gmail.com>
2025-02-21 12:04:22 +0600

    videoes is loaded here from the api

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-// categories section is here 
+// button categories section is here 
 
 function loadCategories (){
     fetch ('https://openapi.programming-hero.com/api/phero-tube/categories')
@@ -6,7 +6,7 @@ function loadCategories (){
     .then(data=>displayCategories(data.categories))
 }
 
-// display categories 
+// button display categories 
 
 function displayCategories(categories){
     const buttonContainer = document.getElementById('button-container');
@@ -20,4 +20,43 @@ function displayCategories(categories){
     });
 }
 
+
+// video categories is here 
+
+function videoCategories (){
+    fetch ('https://openapi.programming-hero.com/api/phero-tube/videos')
+    .then(res => res.json())
+    .then(data => displayVideos(data.videos))
+}
+
+function displayVideos (videos){
+    const videoContainer = document.getElementById('video-container')
+    // console.log(videos)
+    for(const video of videos){
+        // console.log(video)
+        const div = document.createElement('div');
+        div.classList = 'card';
+        div.innerHTML = `
+            <figure>
+                <img class = "h-[200px]"
+                src="${video.thumbnail}" />
+            </figure>
+            <div class="card-body">
+                <h2 class="card-title">
+                Shoes!
+                <div class="badge badge-secondary">NEW</div>
+                </h2>
+                <p>If a dog chews shoes whose shoes does he choose?</p>
+                <div class="card-actions justify-end">
+                <div class="badge badge-outline">Fashion</div>
+                <div class="badge badge-outline">Products</div>
+                </div>
+            </div>
+        `
+        videoContainer.appendChild(div)
+    }
+
+}
+
 loadCategories();
+videoCategories()

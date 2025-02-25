cc75009484d05ee282a7f6abc92f4a987730844b
Rajia Sultana Shraboni <shraboni3103@gmail.com>
2025-02-25 15:53:30 +0600

    updated

--- test.js
+++ test.js
@@ -1,18 +1,18 @@
-function getTimeString(time) {
-    const year = Math.floor(time / (3600 * 24 * 365)); 
-    const remainingAfterYear = time % (3600 * 24 * 365); 
+// function getTimeString(time) {
+//     const year = Math.floor(time / (3600 * 24 * 365)); 
+//     const remainingAfterYear = time % (3600 * 24 * 365); 
 
-    const day = Math.floor(remainingAfterYear / (3600 * 24)); 
-    const remainingAfterDay = remainingAfterYear % (3600 * 24); 
+//     const day = Math.floor(remainingAfterYear / (3600 * 24)); 
+//     const remainingAfterDay = remainingAfterYear % (3600 * 24); 
 
-    const hour = Math.floor(remainingAfterDay / 3600); 
-    const remainingSeconds = remainingAfterDay % 3600; 
+//     const hour = Math.floor(remainingAfterDay / 3600); 
+//     const remainingSeconds = remainingAfterDay % 3600; 
 
-    const minute = Math.floor(remainingSeconds / 60); 
-    const second = remainingSeconds % 60; 
+//     const minute = Math.floor(remainingSeconds / 60); 
+//     const second = remainingSeconds % 60; 
 
-    return `${year}y ${day}d ${hour}h ${minute}m ${second}s`; 
-}
+//     return `${year}y ${day}d ${hour}h ${minute}m ${second}s`; 
+// }
 
-const result = getTimeString(25000000); 
-console.log(result);
+// const result = getTimeString(25000000); 
+// console.log(result);
